1fbcee672a35a3899ef25c6765fc3e6653c399df
jan kowalski <kowalski@wp.pl>
2019-11-17 11:54:47 +0100

    rectangle impl. 2.0

--- src/DrawableMain.java
+++ src/DrawableMain.java
@@ -1,17 +1,7 @@
 public class DrawableMain {
 
     public static void main(String[] args) {
-
-        int side = 5;
-
-        for (int i = 0; i < side; i++) {
-            int j = 0;
-            if (i <= j) {
-                System.out.print('*');
-                j++;
-            }
-        }
-
-
+        Rectangle rectangle = new Rectangle();
+        rectangle.draw();
     }
 }
